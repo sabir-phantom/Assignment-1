e806614e95457e17de5420e0b704a2c07d07af49
sabir-phantom <sabir.sbair@gmail.com>
2024-01-09 22:13:39 +0600

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 
 function age() {
     let age = parseInt(document.getElementById('age').value);
+    let child = document.getElementById('child-card');
+    let adult = document.getElementById('adult-card');
 
-    if (age < 18) {
-        alert("You're a child");
+    if (age < 18 && age > 0) {
+        // alert("You're a child");
+        child.style.display = "block";
+        adult.style.display = "none";
+
     }
     else if (age >= 18 ) {
-        alert("You're an adult");
+        adult.style.display = "block";
+        child.style.display = "none";
+    }
+    else if (age == 0) {
+        alert("Please enter a valid age");
     }
 
     console.log("age: " + age);
