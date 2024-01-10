38dc4ff57459dff1fa9817bced8b7ac42098879d
sabir-phantom <sabir.sbair@gmail.com>
2024-01-11 01:03:29 +0600

    created new branch for further fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,30 @@ function age() {
     console.log("age: " + age);
 }
 
+// multiplication table
+// function table1(){
+//     for(let i = 1; i <= 10; i++) {
+//         document.write("<br>");
+//         for(let j = 1; j <= 10; j++) {
+//             // row += `${i*j}\t`;
+//             console.log(i + " * " + j + " = " + i * j);
+//         }
+//         // console.log(row);
+//     }  
+// }
+
+
+// vowels
+// function vowelsCount(strng) {
+//     const vowels = ["a", "e", "i", "o", "u"];
+//     let countVowels = 0;
+
+//     for (char in strng) {
+//         if (vowels.includes(strng[char])) {
+//             countVowels++;
+//         }
+//     }
+//     console.log(countVowels);
+// }
+
+// vowelsCount("A brown fox jumped over the lazy dog");
